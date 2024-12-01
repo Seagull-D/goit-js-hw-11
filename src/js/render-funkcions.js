d868b902f90d2e37e8efb9e4f29998dfe979c5c1
Dmytro Chaika <djseagull90@gmail.com>
2024-12-01 12:19:52 +0100

    added correct slbx

--- src/js/render-funkcions.js
+++ src/js/render-funkcions.js
@@ -3,14 +3,14 @@ export default createMurkup;
 function createMurkup(arr) {
 
     return arr.map(({webformatURL,largeImageURL,tags,likes,views,comments,downloads }) => `<li class="list-item" >
-  <a href="${largeImageURL}" alt="${tags}" title=""/></a>
+  <a href="${largeImageURL}" alt="${tags}" title=""/>
     <img src="${webformatURL}" alt="${tags}" class="img-item">
   <div class="list-container">
   <p class="item">likes <span>${likes}</span></p>
   <p class="item">Wiews <span>${views}</span></p>
   <p class="item">Comments <span>${comments}</span></p>
   <p class="item">Downlods <span>${downloads}</span></p>
-  </div>
+  </div></a>
 </li>`).join("")
     
 } 
