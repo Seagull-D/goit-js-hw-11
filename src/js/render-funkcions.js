eef4c57855144d0e9427acc66c82bfe18b5f67e6
Dmytro Chaika <djseagull90@gmail.com>
2024-12-01 11:11:43 +0100

    add SimpleLightbox

--- src/js/render-funkcions.js
+++ src/js/render-funkcions.js
@@ -1,15 +1,16 @@
 export default createMurkup;
 
 function createMurkup(arr) {
-    console.log(arr)
+
     return arr.map(({webformatURL,largeImageURL,tags,likes,views,comments,downloads }) => `<li class="list-item" >
-  <img src="${webformatURL}" alt="${tags}" class="img-item">
+  <a href="${largeImageURL}" alt="${tags}" title=""/></a>
+    <img src="${webformatURL}" alt="${tags}" class="img-item">
   <div class="list-container">
   <p class="item">likes <span>${likes}</span></p>
   <p class="item">Wiews <span>${views}</span></p>
   <p class="item">Comments <span>${comments}</span></p>
   <p class="item">Downlods <span>${downloads}</span></p>
   </div>
-</li>`)
+</li>`).join("")
     
 } 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-`use strict`
+"use strict"
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 import getPictures from "./js/pixabay-apy";
 import createMurkup from "./js/render-funkcions";
 
 const form = document.querySelector(".form-js");
-const list = document.querySelector(".list-js"
-)
+const list = document.querySelector(".list-js");
+
 
 
 form.addEventListener("submit", toSabmit);
@@ -13,10 +17,28 @@ function toSabmit(evt) {
     evt.preventDefault();
     const {picture} = evt.target.elements
     getPictures(picture.value)
-    .then((data) => (list.innerHTML = createMurkup(data.hits)))
+        .then((data) => {
+            if (!data.hits.length) { iziToast.show({
+      message: "Sorry, there are no images matching your search query. Please try again!",
+      position: "topRight",
+      color: "red"
+            })
+                
+            }
+            else {
+          
+
+                list.innerHTML = createMurkup(data.hits)
+                }           
+            
+         })
         .catch((error) => console.log(error.message))
-    .finally(() => picture.value = "" )
+        .finally(() => {
+            picture.value = "" 
+            litebox.refresh()
+        })
 }
 
 
+const litebox = new SimpleLightbox('.list-js a', { /* options */ });
 
