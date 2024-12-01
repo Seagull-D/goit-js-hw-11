1541f641aa1b5c2d7e3c1a44a69bcf2666c051e1
Dmytro Chaika <djseagull90@gmail.com>
2024-12-01 15:43:22 +0100

    wsd

--- src/main.js
+++ src/main.js
@@ -24,6 +24,7 @@ function toSabmit(evt) {
         console.dir(picture.value)
       
     }
+    list.innerHTML = ""; 
 loader.classList.remove("hidden");
     getPictures(picture.value)
             .then((data) => {
